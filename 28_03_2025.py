eb9a5a4137f83e54b190e54cfe62d567bc0350fd
dineshkumar24v <dineshkumar24v@gmail.com>
2025-04-02 01:07:56 +0530

    Add files via upload

---
+++ 28_03_2025.py
@@ -0,0 +1,70 @@
+
+
+'''prime number question'''
+
+# Prime numbaer = A number having only 2 factors 
+
+# Method 1:
+
+num1=27
+count =0
+for i in range(1, num1+1):
+  if num1 % i == 0:
+    print(i)
+    count += 1
+if count ==2:
+  print('prime')
+else:
+  print('not prime')
+
+# print('prime number') if count == 2 else print('not  a prime number')
+
+
+# Method 2:
+
+num1 = 9
+spy = False 
+
+for i in range(2, num1):
+  if num1 % 1 == 0:
+    spy = True
+    print('It is not a prime')
+    break
+if spy == False:
+  print('prime number')
+else: 
+  print('not a prime number')
+
+# Method 3:
+
+num1 = 9
+spy = False 
+
+for i in range(2, num1//2+1):   # 2 to 12
+  if num1 % 1 == 0:
+    spy = True
+    print('It is not a prime')
+    break
+if spy == False:
+  print('prime number')
+else: 
+  print('not a prime number')
+
+# Method 4:  is most efficient as it will have less no of iterations
+
+num1 = 9
+spy = False 
+
+for i in range(2, int(num1 ** 0.5)+1):   # 2 to 12
+  if num1 % 1 == 0:
+    spy = True
+    print('It is not a prime')
+    break
+if spy == False:
+  print('prime number')
+else: 
+  print('not a prime number')
+
+
+
+
